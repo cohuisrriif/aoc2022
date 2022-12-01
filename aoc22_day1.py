ac2820078a94879495dd37e637606e8422ac3548
cohuisrriif <chirif.ousri@yahoo.fr>
2022-12-01 21:57:08 +0100

    formating folder and code

--- aoc22_day1.py
+++ aoc22_day1.py
@@ -5,17 +5,17 @@ from itertools import groupby
 # phase 1
 
 ## test 
-with open('./input_day1_test.txt') as f:
+with open('./input/input_day1_test.txt') as f:
     data = f.read().split('\n')
 
 data_test = [int(calories) if calories != '' else '' for calories in data]
 data_test = [sum(list(calories)) for emptychar, calories in groupby(data_test, key=bool) if emptychar]
 
 solution_part1_test = max(data_test)
-print(f'solution test part 1 : {solution_part1_test}')
+print(f'solution day 1 test part 1 : {solution_part1_test}')
 
 ## submission 
-with open('./input_day1_1.txt') as f:
+with open('./input/input_day1.txt') as f:
     data = f.read().split('\n')
     
 data = [int(calories) if calories != '' else '' for calories in data]
@@ -29,19 +29,9 @@ print(f'solution day 1 part 1 : {solution_part1_day1}')
 ## test 
 data_test.sort()
 solution_part2_test = sum(data_test[-3:])
-print(f'solution test part 1 : {solution_part2_test}')
+print(f'solution day 1 test part 2 : {solution_part2_test}')
 
 ## submission 
 data.sort()
 solution_part2_day1 = sum(data[-3:])
-print(f'solution test part 1 : {solution_part2_day1}')
-
-
-
-
-
-
-
-
-
-
+print(f'solution day 1  part 2 : {solution_part2_day1}')
