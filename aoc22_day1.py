ebadf1563e3b7e04a9d03c5a56ad7453072d935a
cohuisrriif <chirif.ousri@yahoo.fr>
2022-12-01 21:52:13 +0100

    aoc day 1

--- aoc22_day1.py
+++ aoc22_day1.py
@@ -1,4 +1,47 @@
-# aoc 2022 - day 1 
+# AOC 2022 - DAY 1 
 
-def function(None):
-    return(None)
+from itertools import groupby
+
+# phase 1
+
+## test 
+with open('./input_day1_test.txt') as f:
+    data = f.read().split('\n')
+
+data_test = [int(calories) if calories != '' else '' for calories in data]
+data_test = [sum(list(calories)) for emptychar, calories in groupby(data_test, key=bool) if emptychar]
+
+solution_part1_test = max(data_test)
+print(f'solution test part 1 : {solution_part1_test}')
+
+## submission 
+with open('./input_day1_1.txt') as f:
+    data = f.read().split('\n')
+    
+data = [int(calories) if calories != '' else '' for calories in data]
+data = [sum(list(calories)) for emptychar, calories in groupby(data, key=bool) if emptychar]
+
+solution_part1_day1 = max(data)
+print(f'solution day 1 part 1 : {solution_part1_day1}')
+
+# phase 2
+
+## test 
+data_test.sort()
+solution_part2_test = sum(data_test[-3:])
+print(f'solution test part 1 : {solution_part2_test }')
+
+## submission 
+data.sort()
+solution_part2_day1 = sum(data[-3:])
+print(f'solution test part 1 : {solution_part2_day1}')
+
+
+
+
+
+
+
+
+
+
